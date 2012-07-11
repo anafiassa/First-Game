dfc4f9af4c89f630cb18fa8d22af46cafadf1073
anafiassa <jenofapen@gmail.com>
2012-07-11 12:45:47 -0400

    Initial Project setup

---
+++ src/First_Game/core.clj
@@ -0,0 +1,1 @@
+(ns First-Game.core)
---
+++ test/First_Game/test/core.clj
@@ -0,0 +1,6 @@
+(ns First-Game.test.core
+  (:use [First-Game.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
